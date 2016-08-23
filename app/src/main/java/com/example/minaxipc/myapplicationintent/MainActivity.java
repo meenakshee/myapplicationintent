741f7144fb55eab5abd9235efbef9bdbd65b8f33
meenakshee <durby1892@gmail.com>
2016-08-23 04:44:25 -0700

    Initial commit

---
+++ app/src/main/java/com/example/minaxipc/myapplicationintent/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.minaxipc.myapplicationintent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+      Button b1,b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        b1=(Button)findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("https://www.flipkart.com/"));
+                startActivity(i);
+            }
+        });
+
+        b2=(Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent("com.example.minaxipc.myapplicationintent.LAUNCH",Uri.parse("https://www.flipkart.com"));
+                startActivity(i);
+            }
+        });
+
+
+
+    }
+}
