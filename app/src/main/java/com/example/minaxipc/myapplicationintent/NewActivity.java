a8f55ef98da51957ce12a5d68509896fb93373bd
meenakshee <durby1892@gmail.com>
2016-08-26 03:08:54 -0700

    made some chenges

--- app/src/main/java/com/example/minaxipc/myapplicationintent/NewActivity.java
+++ app/src/main/java/com/example/minaxipc/myapplicationintent/NewActivity.java
@@ -2,6 +2,7 @@ package com.example.minaxipc.myapplicationintent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NewActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class NewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+        TextView mtv = (TextView) findViewById(R.id.tv1);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            String data = bundle.getString("data");
+            int age = bundle.getInt("age", 0);
+            mtv.setText(data + "     Age is   :  " + age);
+        }
     }
 }
